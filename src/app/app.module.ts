dc116786fab9ec7121cc624eda09fb2632a5e3c3
zenojr <zfrancajunior@gmail.com>
2019-10-10 16:30:39 -0300

    new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDividerModule,
+         MatButtonModule,
+         MatToolbarModule,
+         MatIconModule,
+         MatGridListModule,
+         MatCardModule,
+         MatInputModule          } from '@angular/material';
+
+import { FormsModule             } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatGridListModule,
+    MatCardModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
