cbb995b9a1e1aa28dc3b5778dc5874fae0201d13
zenojr <zfrancajunior@gmail.com>
2019-10-11 10:52:08 -0300

    update new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule           } from '@angular/platform-browser';
+import { NgModule                } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule        } from './app-routing.module';
+import { AppComponent            } from './app.component';
+import { LoginComponent          } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDividerModule,
          MatButtonModule,
@@ -24,6 +26,7 @@ import { FormsModule             } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     FormsModule,
     MatDividerModule,
     MatButtonModule,
