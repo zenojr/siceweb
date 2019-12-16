fc8495a5f77215130e7085b394f782d2abde16b1
zenojr <zfrancajunior@gmail.com>
2019-12-16 08:35:58 -0200

    update project

--- src/app/repassadeira/form-rep/form-rep.component.ts
+++ src/app/repassadeira/form-rep/form-rep.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { DialogData } from '../repFormModel';
+import { inject } from '@angular/core/testing';
 
 @Component({
   selector: 'app-form-rep',
@@ -9,12 +10,15 @@ import { DialogData } from '../repFormModel';
 })
 export class FormRepComponent implements OnInit {
 
+  
+
   constructor(
     public repassForm: MatDialogRef<FormRepComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DialogData
   ) { }
 
   ngOnInit() {
+    console.log(this.data['corteRolo'] );
   }
 
   onNoClick(): void {
