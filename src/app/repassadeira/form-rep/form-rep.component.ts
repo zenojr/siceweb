699d0ac0f827c9c70d65e609d560ec25fd14c5a5
zenojr <zfrancajunior@gmail.com>
2020-01-06 14:24:24 -0200

    update project

--- src/app/repassadeira/form-rep/form-rep.component.ts
+++ src/app/repassadeira/form-rep/form-rep.component.ts
@@ -4,11 +4,12 @@ import { DialogData                    } from '../repFormModel';
 import { MatSnackBar                   } from '@angular/material/snack-bar';
 import { DataRepOut                    } from './saveDataModel';
 import { LoginService                  } from '../../login/login.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-form-rep',
+     selector: 'app-form-rep',
   templateUrl: './form-rep.component.html',
-  styleUrls: ['./form-rep.component.scss']
+    styleUrls: ['./form-rep.component.scss']
 })
 export class FormRepComponent implements OnInit {
   indexCorteRol = 0;
@@ -22,17 +23,16 @@ repassadeira: any;
      taraOut: number;
    obsRepass: string;
   constructor(
-    public repassForm: MatDialogRef<FormRepComponent>,
-    public   snackBar: MatSnackBar,
+    public   repassForm: MatDialogRef<FormRepComponent>,
+    public     snackBar: MatSnackBar,
     public loginService: LoginService,
+    public         http: HttpClient,
     @Inject(MAT_DIALOG_DATA) public data: DialogData,
     @Inject(MAT_DIALOG_DATA) public dataRepOut: DataRepOut
   ) { }
 
-  ngOnInit() {
-    console.log(this.data['mmValida'] );
-    this.loginService.currentUser.subscribe( user => this.user = user );
-    // this.loginService.currentRepassa.subscribe( repassa => this.repassadeira = repassa );
+  ngOnInit() {    
+    this.loginService.currentUser.subscribe( user => this.user = user );    
     this.loginService.currentSetor.subscribe( repassa => this.repassadeira = repassa.slice(13));
     console.log( 'inside form' +  this.repassadeira);
   }
@@ -41,21 +41,21 @@ repassadeira: any;
   let numOp       = this.data['op'];
   let itCodigo    = this.data['codProd'];
   let codLote     = this.data['lote']
-  let lance       =  this.data['lance']
-  let tara        =  this.taraOut;
-  let ajSpark     =  this.data['spark']
+  let lance       = this.data['lance']
+  let tara        = this.taraOut;
+  let ajSpark     = this.data['spark']
   let destino     = this.data['cliente']
   let usuario     = this.user;
-  let numRepassa  =  this.repassadeira;
-  let quantMetro  =  this.data['quantEtq']
-  let quantRolo   =  this.data['qtdRolo']
-  let quantRet    =  this.data['qtdRetalho']
-  let quantSuc    =  this.data['qtdSucata']
+  let numRepassa  = this.repassadeira;
+  let quantMetro  = this.data['quantEtq']
+  let quantRolo   = this.data['qtdRolo']
+  let quantRet    = this.data['qtdRetalho']
+  let quantSuc    = this.data['qtdSucata']
   let DevProd     = this.devProd
   let Observ      = this.obsRepass
-  let codImp      =  this.data['codImp'];
-  let codProblema =  this.data['codProblema'];
-  let codProbSuc  =  this.data['codProbSuc'];
+  let codImp      = this.data['codImp'];
+  let codProblema = this.data['codProblema'];
+  let codProbSuc  = this.data['codProbSuc'];
 
   const url = 'http://192.168.0.7:8080/cgi-bin/wspd_cgi.sh/WService=emswebelttst/scb005ws.p';
   let bigStringOut = '';  
@@ -79,8 +79,12 @@ repassadeira: any;
                  'codProblema='+ codProblema+ '&' +
                  'codProbSuc=' + codProbSuc;
 
-  console.log(bigStringOut);
+  this.http.get( bigStringOut, {responseType: 'text'} )
+  .subscribe( response => {
+    console.log( 'DataSend' + response );
 
+  }, error =>  this.error = console.log(error)
+  )
 
  }
 
